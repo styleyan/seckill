497a7666dc907d4b6ba6cade49e0ee159479bb38
yanxiaofei <forget_love2161@126.com>
2019-01-27 23:12:02 +0800

    feat: 完善service层逻辑

--- src/main/java/com/isyxf/service/SeckillService.java
+++ src/main/java/com/isyxf/service/SeckillService.java
@@ -6,6 +6,7 @@ import com.isyxf.entity.Seckill;
 import com.isyxf.exception.RepeatKillExecption;
 import com.isyxf.exception.SeckillCloseExecption;
 import com.isyxf.exception.SeckillException;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,12 +14,14 @@ import java.util.List;
  * 业务接口: 站在 "使用者" 角度设计接口
  * 三个方面: 方法定义粒度，参数，返回类型(return 类型/异常)
  */
-public class SeckillService {
+public abstract class SeckillService {
     /**
      * 查询所有秒杀记录
      * @return
      */
     List<Seckill> getSeckillList();
+
+    public abstract List<Seckill> getSeckillList();
 
     /**
      * 查询单个秒杀记录
@@ -42,5 +45,13 @@ public class SeckillService {
      * @param md5
      */
     SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
+        throws SeckillException, RepeatKillExecption, SeckillCloseExecption;
+
+    public abstract Seckill getById(long seckillId);
+
+    public abstract Exposer exposerSeckillUrl(long seckillId);
+
+    @Transactional
+    public abstract SeckillExecution executionSeckill(long seckillId, long userPhone, String md5)
         throws SeckillException, RepeatKillExecption, SeckillCloseExecption;
 }
--- src/main/java/com/isyxf/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/isyxf/service/impl/SeckillServiceImpl.java
@@ -10,18 +10,26 @@ import com.isyxf.enums.SeckillStatEnum;
 import com.isyxf.exception.RepeatKillExecption;
 import com.isyxf.exception.SeckillCloseExecption;
 import com.isyxf.exception.SeckillException;
+import com.isyxf.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import java.util.Date;
 import java.util.List;
 
-public class SeckillServiceImpl implements SeckillExecution {
+@Service
+public class SeckillServiceImpl implements SeckillService {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    // 注入 Service 依赖
+    @Autowired
     private SeckillDao seckillDao;
 
+    @Autowired
     private SuccessKilledDao successKilledDao;
 
     // md5盐值字符串，用于混淆MD5
@@ -67,10 +75,17 @@ public class SeckillServiceImpl implements SeckillExecution {
     }
 
     @Override
+    @Transactional
+    /**
+     * 使用注解控制事务方法的优点, TODO: 事务
+     * 1: 开发团队达成一致约定，明确标注事务方法的编程风格
+     * 2: 保证事务方法的执行时间尽可能短，不要穿插其他网络操作，RPC/HTTP请求或者玻璃到事务方法外部
+     * 3: 不是所有的方法都需要事务，如只有一条修改操作，或操作不需要事务控制
+     */
     public SeckillExecution executionSeckill(long seckillId, long userPhone, String md5)
         throws SeckillException, RepeatKillExecption, SeckillCloseExecption {
 
-        if (md5 == null || md5.equals(getMD5(seckillId))) {
+        if (md5 == null || !md5.equals(getMD5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
         }
         // 执行秒杀逻辑: 减库存 + 记录购买行为
