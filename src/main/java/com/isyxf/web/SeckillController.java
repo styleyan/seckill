858e0d0f89aa24510bd92a3fa93e576f4202771a
yanxiaofei <forget_love2161@126.com>
2019-03-23 00:38:29 +0800

    feat: 完善接口数据返回

--- src/main/java/com/isyxf/web/SeckillController.java
+++ src/main/java/com/isyxf/web/SeckillController.java
@@ -1,19 +1,21 @@
 package com.isyxf.web;
 
 import com.isyxf.dto.Exposer;
+import com.isyxf.dto.SeckillExecution;
 import com.isyxf.dto.SeckillResult;
 import com.isyxf.entity.Seckill;
+import com.isyxf.enums.SeckillStatEnum;
+import com.isyxf.exception.RepeatKillExecption;
+import com.isyxf.exception.SeckillCloseExecption;
 import com.isyxf.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 // 类似 @Service @Component 放入spring容器当中
@@ -80,5 +82,47 @@ public class SeckillController {
             result = new SeckillResult<Exposer>(false, e.getMessage());
         }
         return result;
+    }
+
+    /**
+     * 秒杀结果
+     * @return
+     */
+    @RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                    @PathVariable("md5") String md5,
+                                                    @CookieValue(value = "killPhone", required = false) Long phone) {
+
+        // 如果没有穿手机号，则直接返回失败
+        if (phone == null) {
+            return new SeckillResult<SeckillExecution>(false, "未注册");
+        }
+
+        SeckillResult<SeckillExecution> result;
+
+        try{
+            SeckillExecution execution = seckillService.executeSeckill(seckillId, phone, md5);
+            return new SeckillResult<SeckillExecution>(true, execution);
+        } catch (RepeatKillExecption e) {
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        } catch (SeckillCloseExecption e) {
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.END);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        }catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        }
+    }
+
+    /**
+     * 获取系统时间
+     */
+    @RequestMapping(value = "/time/now", method = RequestMethod.GET)
+    public SeckillResult<Long> time() {
+        Date now = new Date();
+        return new SeckillResult<>(true, now.getTime());
     }
 }
